b7363bfbb45c2eaf41756fe54be91db7ca516558
sann <diegofernandezp001@gmail.com>
2022-01-27 23:49:09 -0600

    primer commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+import Link from 'next/link'
+
+
+const Home: NextPage = () => {
+  
+
+  return (
+    <div>
+     hola
+    </div>
+  )
+
+}
+
+
+
+export default Home;
