e956e2890bd7618b330cdc67f35b0fa687b143d2
sann <diegofernandezp001@gmail.com>
2022-02-05 20:24:57 -0600

    Se agregan los datos de la base de datos

---
+++ src/utils/data/Home.ts
@@ -0,0 +1,164 @@
+export const DATA_SKILLS = [
+    {
+        img: "https://i.imgur.com/mhj0e4M.png",
+        title: "GITHUB"
+    },
+    {
+        img: "https://i.imgur.com/kzTybrt.png",
+        title: "JAVASCRIPT"
+    },
+    {
+        img: "https://i.imgur.com/qOHa1PR.png",
+        title: "PYTHON"
+    },
+    {
+        img: "https://i.imgur.com/qOHa1PR_d.webp?maxwidth=760&fidelity=grand",
+        title: "LINUX"
+    },
+    {
+        img: "https://i.imgur.com/o5Y18N1_d.webp?maxwidth=760&fidelity=grand",
+        title: "SQL"
+    },
+    {
+        img: "https://i.imgur.com/JkcS9k4.png",
+        title: "HTML"
+    },
+    {
+        img: "https://i.imgur.com/pa84wzE.png",
+        title: "CSS"
+    },
+    {
+        img: "https://i.imgur.com/GI2eV91.png",
+        title: "REACT"
+    },
+    {
+        img:"https://imgur.com/c3mOiSH",
+        title:"NODE"
+    },
+    {
+        img:"https://i.imgur.com/HEZ6Eko.png",
+        title:"GIT"
+    },
+    {
+        img:"https://i.imgur.com/GYAM3SG_d.webp?maxwidth=760&fidelity=grand",
+        title:"DOCKER"
+    },
+    {
+        img:"https://i.imgur.com/8hJ8wCK.png",
+        title:"SASS"
+    },
+    {
+        img:"https://i.imgur.com/eK2x0n5_d.webp?maxwidth=760&fidelity=grand",
+        title:"MONGO"
+    }
+];
+
+export const DATA_NAV = [
+    {
+        home: "Inicio",
+        skills: "Habilidades",
+        portfolio: "Portafolio",
+        contact: "Contacto",
+        lang: "es"
+    },
+    {
+        home: "Home",
+        skills: "Skills",
+        portfolio: "Portfolio",
+        contact: "Contact",
+        lang: "en"
+    }
+];
+
+export const DATA_ABOUTEME = [
+    {
+        presentation:"Soy un desarrollador web, apasionado por la tecnología, tengo más de tres años como desarrollador e incluso actualmente continúo expandiendo mi conocimiento sobre nuevas tecnologías de forma autodidacta",
+        name:"Soy Diego Fernandez",
+        stand: "Desarrollador Full Stack ",
+        lang: "es"
+    },
+    {
+        presentation: "I’m a web developer, passionate about technology, I have more than three years as a developer and even currently I continue to expand my knowledge about new technologies autodidactically",
+        name: "I'm Diego Fernandez",
+        stand: "Full Stack Developer",
+        lang: "en"
+    }
+];
+
+export const DATA_PORTFOLIO = [
+    {
+        name: "portfolio",
+        img:"https://i.imgur.com/dRH6z3n.png",
+        title:"Mi Portafolio",
+        text:"El proyecto que es mi carta de presentación sin duda tiene que estar en mi portafolio de proyectos. En su código fuente se puede ver claramente cuanto se del tema y este portafolio ira creciendo en proporción a mis conocimientos y proyectos",
+        more:"https://github.com/diegoufp/portfolio",
+        lang: "es"
+    },
+    {
+        name: "portfolio",
+        img:"https://i.imgur.com/dRH6z3n.png",
+        title:"My Portfolio",
+        text:"The project that is my cover letter certainly has to be in my portfolio of projects. In its source code you can clearly see how much of the topic and this portfolio will be growing in proportion to my knowledge and projects",
+        more:"https://github.com/diegoufp/portfolio",
+        lang: "en"
+    }
+];
+
+export const DATA_REQUIRED = [
+    {
+        form: "contact", 
+        name:"El campo 'Nombre' es requerido",
+        email: "El campo 'Email' es requerido",
+        subject:"El campo 'Asunto a tratar' es requerido",
+        comments:"El campo 'Comentarios' es requerido", 
+        lang: "es", 
+    },
+    {
+        form: "contact", 
+        name:"The 'Name' field is required",
+        email:"The 'Email' field is required",
+        subject:"The field 'Subject to be addressed' is required",
+        comments:"The 'Comments' field is required",
+        lang: "en", 
+    }
+];
+
+export const DATA_REGEX = [
+    {
+        form: "contact", 
+        name: "El campo 'Nombre' solo acepta letras y espacios en blanco",
+        email:"El campo 'Email' es incorrecto",
+        comments:"El campo 'Comentarios' no debe exceder los 255 caracteres",
+        subject:"",
+        lang: "es", 
+    },
+    {
+        form: "contact", 
+        name:"The 'Name' field only accepts letters and blanks",
+        email:"The 'Email' field is incorrect",
+        comments:"The 'Comments' field must not exceed 255 characters",
+        subject:"",
+        lang: "en", 
+    }
+];
+
+export const DATA_PLACEHOLDER = [
+    {
+        form: "contact", 
+        name:"Escribe tu nombre",
+        email:"Escribe tu email",
+        subject:"Asunto a tratar",
+        comments:"Escribe tus comentarios",
+        submit:"Enviar",
+        lang: "es", 
+    },
+    {
+        form: "contact", 
+        name:"Enter your name",
+        email:"Write your email",
+        subject:"Issue at hand",
+        comments:"Write your comments",
+        submit:"Submit",
+        lang: "es", 
+    }
+];
